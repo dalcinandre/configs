d8c3087b8b538ccd1c176d37bdc9abf64a9bd320
dalcinandre <andre.programmer@gmail.com>
2021-11-08 14:41:23 -0300

    alteracoes para resolver coisas

--- i3/monitors.sh
+++ i3/monitors.sh
@@ -2,4 +2,16 @@
 #!/usr/bin/env bash
 #
 
-xrandr --auto --output VGA-1 --left-of LVDS-1 --output HDMI-1 --right-of LVDS-1 --rotate right --mode 1920x1080
+VGA=VGA-1
+NOTE=LVDS-1
+HDMI=HDMI-1
+
+# xrandr --auto --output VGA-1 --left-of LVDS-1 --output HDMI-1 --right-of LVDS-1 --rotate right --mode 1920x1080
+#xrandr --auto \
+#  --output VGA-1 --right-of LVDS-1 --mode 1920x1080 \
+#  --output HDMI-1 --right-of LVDS-1 --rotate right --mode 1920x1080
+
+xrandr --auto \
+  --output ${NOTE}  --mode 1366x768     --rotate normal                     \
+  --output ${VGA}   --mode 1920x1080    --rotate normal --right-of ${NOTE}  \
+  --output ${HDMI}  --mode 1920x1080    --rotate right  --right-of ${VGA}
