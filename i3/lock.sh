ac801c741f2b73413329dc6ac8cea8a0e6818784
André Dalcin <andre.programmer@gmail.com>
2022-10-22 10:14:32 -0300

    alteracoes nos arquivos

--- i3/lock.sh
+++ i3/lock.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-img="$(mktemp).png"
+#img="$(mktemp).png"
 # icon="${HOME}/.dotfiles/i3/lock.png"
 
-scrot -q 10 ${img} && \
-  convert ${img} -blur 0x10 ${img} && \
-  # convert ${img} ${icon} -gravity center -composite ${img} && \
-  # feh ${img}
-  i3lock -e -f -n -i ${img}
+#scrot -q 10 ${img} && \
+#  convert ${img} -blur 0x10 ${img} && \
+#  # convert ${img} ${icon} -gravity center -composite ${img} && \
+#  # feh ${img}
+#  i3lock -e -f -n -i ${img}
+#
+i3lock -e -f -n -c 000000
